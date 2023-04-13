ed065935cf0589526dc45412d571ee9135553948
Mun Max <firefox36034@gmail.com>
2023-04-13 16:20:59 +0300

    added static

--- blog/settings.py
+++ blog/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
